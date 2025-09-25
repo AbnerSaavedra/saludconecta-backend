faa5eeea0fef10517ee72a546287b73d625560d2
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2025-09-24 22:36:45 -0400

    Para ver payload

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -13,6 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
+    console.log('Payload JWT recibido:', payload);
     return { id: payload.sub, email: payload.email, roles: payload.roles };
   }
 }
